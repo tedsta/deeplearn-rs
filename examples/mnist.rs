cac2f51a34d6202f25af141fb20ae36649a15779
Theodore DeRego <tderego94@gmail.com>
2016-02-11 18:07:19 -1000

    MNIST validation

--- examples/mnist.rs
+++ examples/mnist.rs
@@ -18,13 +18,23 @@ use deeplearn::init;
 use matrix::Matrix;
 
 fn main() {
+    // Training data
     println!("Reading training labels...");
-    let train_labels = read_mnist_labels("data/mnist/train-labels-idx1-ubyte").unwrap();
+    let train_labels = read_mnist_labels("data/mnist/train-labels-idx1-ubyte", None).unwrap();
     let train_labels_logits: Vec<Matrix<f32>> = train_labels.iter().cloned().map(|x| one_hot(x, 10)).collect();
     println!("Label count: {}", train_labels.len());
 
     println!("Reading training images...");
-    let (rows, columns, train_images) = read_mnist_images("data/mnist/train-images-idx3-ubyte").unwrap();
+    let (rows, columns, train_images) = read_mnist_images("data/mnist/train-images-idx3-ubyte", None).unwrap();
+
+    // Validation data
+    println!("Reading validation labels...");
+    let val_labels = read_mnist_labels("data/mnist/t10k-labels-idx1-ubyte", Some(1000)).unwrap();
+    let val_labels_logits: Vec<Matrix<f32>> = val_labels.iter().cloned().map(|x| one_hot(x, 10)).collect();
+    println!("Label count: {}", train_labels.len());
+
+    println!("Reading validation images...");
+    let (_, _, val_images) = read_mnist_images("data/mnist/t10k-images-idx3-ubyte", Some(1000)).unwrap();
 
     let ctx = matrix::Context::new();
 
@@ -113,9 +123,9 @@ fn main() {
     loss_d.get(&graph).set(&ctx, &loss_d_cpu);
 
     /////////////////////////
-    // Run/Train the network
+    // Train the network
     let mut correct = 0;
-    for epoch in 0..100000 {
+    for epoch in 0..60000 {
         // Upload training data
         let train_sample = epoch%train_images.len();
         input.get(&graph).set(&ctx, &train_images[train_sample]);
@@ -160,9 +170,52 @@ fn main() {
             correct = 0;
         }
     }
+
+    /////////////////////////
+    // Validate the network
+    println!("#######################################");
+    println!("Validating");
+    let mut correct = 0;
+    for epoch in 0..1000 {
+        // Upload training data
+        let train_sample = epoch%val_images.len();
+        input.get(&graph).set(&ctx, &val_images[train_sample]);
+        train_out.get(&graph).set(&ctx, &val_labels_logits[train_sample]);
+
+        graph.run(&ctx);
+        let out = l2_relu_out.get(&graph).get(&ctx);
+        let out_d = l2_relu.get(&graph)
+                           .out_grad[0]
+                           .gradient()
+                           .get(&graph)
+                           .get(&ctx);
+        let l = loss_out.get(&graph).get(&ctx);
+
+        let (mut max_index, mut max_value) = (0, out[(0, 0)]);
+        for (i, val) in out.buffer().iter().enumerate() {
+            if *val > max_value {
+                max_index = i;
+                max_value = *val;
+            }
+        }
+
+        if max_index == val_labels[train_sample] as usize {
+            correct += 1;
+        }
+
+        if epoch % 1000 == 999 {
+            println!("===================");
+            println!("Epoch: {}", epoch);
+            println!("out = {:?}", out);
+            println!("out_d = {:?}", out_d);
+            println!("loss = {:?}", l);
+            println!("Accuracy: {}%", (correct as f32)/(1000 as f32) * 100.0);
+            correct = 0;
+        }
+    }
 }
 
-fn read_mnist_labels<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
+fn read_mnist_labels<P: AsRef<Path>>(path: P, num_samples: Option<usize>) -> io::Result<Vec<u8>> {
     use std::io::{Error, ErrorKind};
 
     let ref mut file = BufReader::new(File::open(path).unwrap());
@@ -177,14 +230,19 @@ fn read_mnist_labels<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
     let label_count = u32::from_be(try!(read_u32(file)));
 
     let mut labels = Vec::with_capacity(label_count as usize);
-    for _ in 0..label_count {
+    for i in 0..label_count {
+        if let Some(num_samples) = num_samples {
+            if i as usize > num_samples {
+                break;
+            }
+        }
         labels.push(try!(read_u8(file)));
     }
 
     Ok(labels)
 }
 
-fn read_mnist_images<P: AsRef<Path>>(path: P) -> io::Result<(usize, usize, Vec<Matrix<f32>>)> {
+fn read_mnist_images<P: AsRef<Path>>(path: P, num_samples: Option<usize>) -> io::Result<(usize, usize, Vec<Matrix<f32>>)> {
     use std::io::{Error, ErrorKind};
 
     let ref mut file = BufReader::new(File::open(path).unwrap());
@@ -201,7 +259,12 @@ fn read_mnist_images<P: AsRef<Path>>(path: P) -> io::Result<(usize, usize, Vec<M
     let columns = u32::from_be(try!(read_u32(file))) as usize;
 
     let mut images = Vec::with_capacity(image_count);
-    for _ in 0..image_count {
+    for i in 0..image_count {
+        if let Some(num_samples) = num_samples {
+            if i as usize > num_samples {
+                break;
+            }
+        }
         let mut pixel_buf = vec![0u8; rows*columns];
         try!(file.read_exact(pixel_buf.as_mut()));
         let matrix = Matrix::from_vec(rows, columns,
