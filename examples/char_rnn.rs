541b007a314d24458afd11f9755e03ede3aeb562
Theodore DeRego <tderego94@gmail.com>
2016-06-01 21:15:17 -1000

    Training and validating in char_rnn example

--- examples/char_rnn.rs
+++ examples/char_rnn.rs
@@ -6,20 +6,22 @@ use std::fs::File;
 use std::io::{
     self,
     BufReader,
-    Read,
+    Write,
 };
 use std::path::Path;
 use std::rc::Rc;
 
-use deeplearn::{init, layers, util, Graph, Trainer};
+use deeplearn::{init, layers, util, Graph};
 use deeplearn::op::Relu;
 use ga::Array;
 
 fn main() {
     let batch_size = 1;
 
-    let (char_map, lines) = load_char_rnn_data("data/bible.txt").unwrap();
+    let (char_map, rev_char_map, lines) = load_char_rnn_data("data/bible.txt").unwrap();
+    let char_classes = char_map.len();
     println!("Loaded char rnn data");
+    println!("Character types: {}", char_classes);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Build the graph
@@ -28,38 +30,90 @@ fn main() {
     let ref mut graph = Graph::new(ctx.clone());
 
     // Input. 1 batch of rows*columns inputs
-    let input = graph.add_variable(vec![batch_size, 26], false, 0.0);
+    let input = graph.add_variable(vec![batch_size, char_classes], false, 0.0);
 
     // Layer 1
-    // Biased fully connected layer with 300 neurons and ReLU activation
-    let (l1_fcb, _, _) = layers::dense_biased(graph, input, 300,
-                                              init::Normal(0.001, 0.005),  // Weights initializer
-                                              init::Normal(0.001, 0.005)); // Bias initializer
-    let l1_out = layers::activation(graph, Relu(l1_fcb));
+    // LSTM layer with 200 cells
+    let (l1_out, _) = layers::lstm(graph, input, 200, init::Normal(0.001, 0.005));
 
     // Layer 2
-    // LSTM layer with 100 cells
-    let (l2_out, _) = layers::lstm(graph, l1_out, 100, init::Normal(0.001, 0.005));
+    // LSTM layer with 200 cells
+    let (l2_out, _) = layers::lstm(graph, l1_out, 200, init::Normal(0.001, 0.005));
 
     // Layer 3
     // Biased fully connected layer with 26 neurons and ReLU activation
-    let (l3_fcb, _, _) = layers::dense_biased(graph, l2_out, 26,
+    let (l3_fcb, _, _) = layers::dense_biased(graph, l2_out, char_classes,
                                               init::Normal(0.001, 0.005),  // Weights initializer
                                               init::Normal(0.001, 0.005)); // Bias initializer
     let l3_out = layers::activation(graph, Relu(l3_fcb));
 
     // Loss
-    let (loss_out, train_out) = layers::mse(graph, l2_out);
+    let (loss_out, train_out) = layers::mse(graph, l3_out);
     let loss_d = graph.add_gradient(loss_out); // Create a gradient to apply to the loss function
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Train and validate the network
 
-    // TODO
+    // We apply a gradient of -0.001 to the loss function
+    let loss_d_cpu = Array::new(vec![batch_size, char_classes], -0.001);
+    loss_d.write(graph, &loss_d_cpu);
+
+    for line in &lines {
+        for t in 1..line.len() {
+            input.write(graph, &char_map[&line[t-1]]);
+            train_out.write(graph, &char_map[&line[t]]);
+            graph.forward();
+        }
+        for t in (1..line.len()).rev() {
+            graph.backward();
+        }
+    }
+
+    let mut l3_out_cpu = Array::new(vec![batch_size, char_classes], 0.0);
+    let mut argmax_out = Array::new(vec![batch_size], 0usize);
+
+    loop {
+        // Print prompt
+        print!(">");
+        io::stdout().flush().unwrap();
+
+        // Get seed string
+        let mut seed = String::new();
+        io::stdin().read_line(&mut seed).unwrap();
+
+        // Input the seed string
+        let mut last_char = 0u8;
+        for c in seed.trim_right().as_bytes() {
+            print!("{}", *c as char);
+            input.write(graph, &char_map[c]);
+            graph.forward();
+            l3_out.read(graph, &mut l3_out_cpu);
+            util::argmax_rows(&l3_out_cpu, &mut argmax_out);
+            last_char = rev_char_map[&argmax_out[&[0]]];
+        }
+
+        // Generate the rest of the output
+        for _ in 0..150 {
+            print!("{}", last_char as char);
+            io::stdout().flush().unwrap();
+
+            input.write(graph, &char_map[&last_char]);
+            graph.forward();
+            l3_out.read(graph, &mut l3_out_cpu);
+            //println!("{:?}", l3_out_cpu);
+            util::argmax_rows(&l3_out_cpu, &mut argmax_out);
+            last_char = rev_char_map[&argmax_out[&[0]]];
+
+            if last_char == b'\n' {
+                break;
+            }
+        }
+        println!("");
+    }
 }
 
 pub fn load_char_rnn_data<P: AsRef<Path>>(path: P)
-    -> io::Result<(HashMap<u8, Array<f32>>, Vec<Vec<u8>>)>
+    -> io::Result<(HashMap<u8, Array<f32>>, HashMap<usize, u8>, Vec<Vec<u8>>)>
 {
     use std::io::BufRead;
 
@@ -69,7 +123,7 @@ pub fn load_char_rnn_data<P: AsRef<Path>>(path: P)
     let mut lines = vec![];
 
     for line in file.lines() {
-        let line: String = try!(line);
+        let line: String = try!(line) + "\n";
         for c in line.as_bytes() {
             unique_chars.insert(*c);
         }
@@ -78,9 +132,11 @@ pub fn load_char_rnn_data<P: AsRef<Path>>(path: P)
 
     let char_classes = unique_chars.len();
     let mut char_map = HashMap::new();
+    let mut rev_char_map = HashMap::new();
     for (i, c) in unique_chars.into_iter().enumerate() {
         char_map.insert(c, util::one_hot_row(i, char_classes));
+        rev_char_map.insert(i, c);
     }
 
-    Ok((char_map, lines))
+    Ok((char_map, rev_char_map, lines))
 }
